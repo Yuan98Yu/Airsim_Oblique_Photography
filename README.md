1be418a2d9084828d649a4cb17ff9cafd137282c
coola <COOLA.LAB@gmail.com>
2021-04-21 11:30:43 +0800

    1. fix a fatal error; 2. Complete Readme.md; 3. add configs/city.json

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # MyScan
 本项目的目标是在 Airsim 中，模拟倾斜摄影进行图像采集。
+
+该项目目前只在 Airsim 提供的 CityEnviron 虚拟环境中进行过测试。
+
+## install
+
+本项目依赖于 Airsim 提供的 python api 以及 Airsim 提供好的虚拟环境的 release 版本。
+
+本项目的安装分为三部分：
+1. Airsim 的 python api, 可参考 [Airsim 官方文档](https://microsoft.github.io/AirSim/apis/)
+   
+   对于 windows 用户，推荐先安装 anonoconda 作为包管理器。在激活 conda 虚拟环境之后，按照下述步骤安装 Airsim 的 python api
+   
+   ```bash
+   pip install msgpack-rpc-python
+   pip install airsim
+   ```
+
+2. 下载及解压 Airsim 提供的虚拟环境的 Release 版本，[Airsim Releases](https://github.com/Microsoft/AirSim/releases)
+
+    提醒：本项目目前仅使用 v1.4.0 - Windows 中的 CityEnviron 进行过测试。
+
+3. 使用本项目根目录下的 airsim_settings 的内容替换 Airsim 的配置文件内容
+
+    对于 Windows 用户，Airsim 的配置文件存放在 ~/Documents/Airsim/settings.json
+
+## run
+
+1. 运行 CityEnviron 环境的可执行文件，等待虚拟环境启动
+
+2. 运行 cv_capture，-s 指定图像保存路径，-c 指定项目的配置文件
+
+    ```bash
+    python cv_capture.py -s ./imgs -c ./configs/city.json
+    ```
--- cv_capture.py
+++ cv_capture.py
@@ -18,6 +18,7 @@ class ImgCapture:
         self.client = airsim.VehicleClient()
         self.client.confirmConnection()
         self.pos_list = self.generate_path(self.cfg)
+        self.camera_list = ["Down", "Front", "Back", "Left", "Right"]
 
     def reset(self):
         # currently reset() doesn't work in CV mode. Below is the workaround
@@ -25,13 +26,17 @@ class ImgCapture:
             airsim.Pose(airsim.Vector3r(0, 0, 0),
                         airsim.to_quaternion(0, 0, 0)), True)
 
-    def navigate_and_catch_imgs(self, save_dir, pos_list):
+    def navigate_and_catch_imgs(
+            self,
+            save_dir,
+            pos_list,
+            camera_list=["Down", "Front", "Back", "Left", "Right"]):
         os.makedirs(os.path.join(save_dir), exist_ok=False)
         pass
         for idx, pos in enumerate(pos_list):
-            self.catch_and_save_imgs(pos, idx, save_dir)
+            self.catch_and_save_imgs(pos, idx, save_dir, camera_list)
 
-    def generate_path(self, cfg):
+    def generate_path(self, cfg, heading_step=None, side_step=None):
         target_region, height, side_overlap_rate, heading_overlap_rate = cfg[
             'target_region'], cfg['height'], cfg['side_overlap_rate'], cfg[
                 'heading_overlap_rate']
@@ -42,42 +47,47 @@ class ImgCapture:
         FOV_degree = 90
         focal_length = 0.02
         frame_size = (0.02, 0.02)
-        heading_step = self._get_step(height+150, frame_size[0], focal_length,
-                                      heading_overlap_rate)
-        side_step = self._get_step(height+150, frame_size[1], focal_length,
-                                   side_overlap_rate)
-        print(f'heading_step: {heading_step}\n'
-              f'side_step: {side_step}')
+        if heading_step is None:
+            heading_step = self._get_step(height + 150, frame_size[0],
+                                          focal_length, heading_overlap_rate)
+        if side_step is None:
+            side_step = self._get_step(height + 150, frame_size[1],
+                                       focal_length, side_overlap_rate)
+        print(f'heading_step: {heading_step}\n' f'side_step: {side_step}')
 
         pos_list = list()
-        heading_num, side_num = int(heading_length / heading_step), int(side_length / side_step)
+        heading_num, side_num = int(heading_length / heading_step), int(
+            side_length / side_step)
         print(f'total_num: {heading_num * side_num}')
-        for i, j in product(range(heading_num), range(side_num)):
-            x, y = target_region[0][0] + i * heading_step, target_region[0][
-                1] + i * side_step
-            pos_list.append((x, y, height))
+        for i in range(heading_num):
+            
+            for j in range(side_num):
+                x, y = target_region[0][0] + i * heading_step, target_region[0][
+                    1] + j * side_step
+                pos_list.append((x, y, height))
         return pos_list
 
-    def catch_and_save_imgs(self, pos, idx, save_dir):
-        print(idx)
+    def catch_and_save_imgs(
+            self,
+            pos,
+            idx,
+            save_dir,
+            camera_list=["Down", "Front", "Back", "Left", "Right"]):
+        print(f'pos {idx}: {pos}')
         self.client.simSetVehiclePose(
-            airsim.Pose(airsim.Vector3r(*pos), airsim.to_quaternion(0, 0, 0)), True)
+            airsim.Pose(airsim.Vector3r(*pos), airsim.to_quaternion(0, 0, 0)),
+            True)
 
         # responses = self.client.simGetImages([
-        #     airsim.ImageRequest("0", airsim.ImageType.Scene),
-        #     airsim.ImageRequest("1", airsim.ImageType.Scene),
-        #     airsim.ImageRequest("2", airsim.ImageType.Scene),
-        #     airsim.ImageRequest("3", airsim.ImageType.Scene),
-        #     airsim.ImageRequest("4", airsim.ImageType.Scene)
+        #     airsim.ImageRequest("Down", airsim.ImageType.Scene),
+        #     airsim.ImageRequest("Front", airsim.ImageType.Scene),
+        #     airsim.ImageRequest("Back", airsim.ImageType.Scene),
+        #     airsim.ImageRequest("Left", airsim.ImageType.Scene),
+        #     airsim.ImageRequest("Right", airsim.ImageType.Scene)
         # ])
-        camera_name_list = ["Down", "Front", "Back", "Left", "Right"]
-
         responses = self.client.simGetImages([
-            airsim.ImageRequest("Down", airsim.ImageType.Scene),
-            airsim.ImageRequest("Front", airsim.ImageType.Scene),
-            airsim.ImageRequest("Back", airsim.ImageType.Scene),
-            airsim.ImageRequest("Left", airsim.ImageType.Scene),
-            airsim.ImageRequest("Right", airsim.ImageType.Scene)
+            airsim.ImageRequest(camera_name, airsim.ImageType.Scene)
+            for camera_name in camera_list
         ])
 
         for i, response in enumerate(responses):
@@ -85,13 +95,13 @@ class ImgCapture:
                 # print("pos\n%s" % (pprint.pformat(response.camera_position)))
                 airsim.write_pfm(
                     os.path.normpath(
-                        os.path.join(save_dir, f'{idx}_{camera_name_list[i]}.pfm')),
+                        os.path.join(save_dir, f'{idx}_{camera_list[i]}.pfm')),
                     airsim.get_pfm_array(response))
             else:
                 # print("pos\n%s" % (pprint.pformat(response.camera_position)))
                 airsim.write_file(
                     os.path.normpath(
-                        os.path.join(save_dir, f'{idx}_{camera_name_list[i]}.png')),
+                        os.path.join(save_dir, f'{idx}_{camera_list[i]}_x{pos[0]}_y{pos[1]}.png')),
                     response.image_data_uint8)
 
     def _get_step(self, height, frame_length, focal_length, ovellap_ratio):
@@ -100,11 +110,30 @@ class ImgCapture:
         return step
 
 
+def collect_imgs(args):
+    capture = ImgCapture(cfg_path=args.cfg_path)
+    capture.navigate_and_catch_imgs(args.save_dir, capture.pos_list,
+                                    capture.camera_list)
+
+
+def test(args):
+    capture = ImgCapture(cfg_path=args.cfg_path)
+    # region, height = capture.cfg['target_region'], capture.cfg['height']
+    # path = list(map(lambda pos: (*pos, height), region))
+    path = capture.generate_path(capture.cfg, heading_step=100, side_step=100)
+    print(path)
+    # capture.navigate_and_catch_imgs(args.save_dir, path, capture.camera_list)
+    camera_list = ['Down']
+    capture.navigate_and_catch_imgs(args.save_dir, path, camera_list)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--test', '-t', action='store_true')
     parser.add_argument('--cfg_path', '-c', type=str, default='./cfg.json')
     parser.add_argument('--save_dir', '-s', type=str, default='./imgs')
     args = parser.parse_args()
-
-    capture = ImgCapture(cfg_path=args.cfg_path)
-    capture.navigate_and_catch_imgs(args.save_dir, capture.pos_list)
+    if args.test:
+        test(args)
+    else:
+        collect_imgs(args)
